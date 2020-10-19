604344f1d2cb774b51ccd0cfbb6c063f9fbea0bd
Mark Blanco <markb1@cmu.edu>
2020-10-19 16:14:31 -0400

    Fix builder script, set correct num trials in sir, add source parsing to sssp.

--- build_things.sh
+++ build_things.sh
@@ -4,12 +4,13 @@ CDIR=$(pwd)
 mkdir ../graphitBUILD
 cd ../graphitBUILD
 cmake $CDIR
+make -j
 
 # Make each algo
 python3 bin/graphitc.py -f $CDIR/apps/sssp_delta_stepping.gt -o sssp.cpp
 python3 bin/graphitc.py -f $CDIR/apps/sir.gt -o sir.cpp
 python3 bin/graphitc.py -f $CDIR/apps/pi.gt -o pi.cpp
-python3 bin/graphitc.py -f $CDIR/apps/pagerank_nd.gt -o pr_nd.cpp
+python3 bin/graphitc.py -f $CDIR/apps/pagrank_nd.gt -o pr_nd.cpp
 
 # Compile
 g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ sssp.cpp 	${CDIR}/apps/sir_helper_fn.cpp -o sssp.exe
@@ -17,4 +18,9 @@ g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ sir.cpp 	${CDI
 g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ pi.cpp 		-o pi.exe
 g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ pr_nd.cpp -o pr.exe
 
-
+cd $CDIR
+mkdir binaries
+cp ../graphitBUILD/pi.exe binaries/
+cp ../graphitBUILD/pr.exe binaries/
+cp ../graphitBUILD/sssp.exe binaries/
+cp ../graphitBUILD/sir.exe binaries/
