2a0c4a1b1cb0d2c5f03bbb6996376446da2e3b02
Mark Blanco <markb1@cmu.edu>
2020-10-19 16:01:59 -0400

    Add builder script

---
+++ build_things.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+CDIR=$(pwd)
+mkdir ../graphitBUILD
+cd ../graphitBUILD
+cmake $CDIR
+
+# Make each algo
+python3 bin/graphitc.py -f $CDIR/apps/sssp_delta_stepping.gt -o sssp.cpp
+python3 bin/graphitc.py -f $CDIR/apps/sir.gt -o sir.cpp
+python3 bin/graphitc.py -f $CDIR/apps/pi.gt -o pi.cpp
+python3 bin/graphitc.py -f $CDIR/apps/pagerank_nd.gt -o pr_nd.cpp
+
+# Compile
+g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ sssp.cpp 	${CDIR}/apps/sir_helper_fn.cpp -o sssp.exe
+g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ sir.cpp 	${CDIR}/apps/sir_helper_fn.cpp -o sir.exe
+g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ pi.cpp 		-o pi.exe
+g++ --std=c++14 -O3 -DOPENMP -fopenmp -I ${CDIR}/src/runtime_lib/ pr_nd.cpp -o pr.exe
+
+
